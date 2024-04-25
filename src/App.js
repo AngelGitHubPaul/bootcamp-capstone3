1057e88711cc471b12f3545b240f4610e568f6ac
angelagnabo22 <angelagnabo22@gmail.com>
2024-04-25 16:01:15 +0800

    Finished register and login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import AppNavBar from './components/AppNavBar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Error from './pages/Error';
+import {UserProvider} from './UserContext'
 
 function App() {
+
+  const [user,setUser] = useState({
+      id: null,
+      isAdmin: null
+  })
+
+  const unsetUser = () => {localStorage.clear()}
+
+  useEffect(() => {
+      fetch(`${process.env.REACT_APP_API_BASE_URL}/users/details`, {
+          headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+          if(data.user) {
+              setUser({
+                  id: data.user._id,
+                  isAdmin: data.user.isAdmin
+              })
+          } else {
+              setUser({
+                  id: null,
+                  isAdmin: null
+              })
+          }
+          
+      })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider value={{user, setUser , unsetUser}}>
+        <Router>
+            <Container fluid>
+                <AppNavBar />
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/register" element={<Register />}/>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/logout" element={<Logout />}/>
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </Container>
+        </Router>
+    </UserProvider>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+
+
+	return(
+		<>
+			<h2> Capstone 3 E-Commerce App</h2>
+		</>
+	)
+}
