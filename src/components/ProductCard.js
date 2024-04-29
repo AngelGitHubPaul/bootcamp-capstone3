3482f7e9a80885663ed39cc251d7fb00b39b1bfa
lovelysendaydiego <lovelysendaydiego@gmail.com>
2024-04-29 11:57:21 +0800

    added product card code

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,29 @@
 import { useState } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Row, Col, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export default function ProductCard ({productProp}) {
+export default function ProductCard({ productProp }) {
 
     const { _id, name, description, price } = productProp;
 
+    return (
 
-	return (
-		<Card id="productComponent1">
-            <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Subtitle>Description:</Card.Subtitle>
-                <Card.Text>{description}</Card.Text>
-                <Card.Subtitle>Price:</Card.Subtitle>
-                <Card.Text>Php {price}</Card.Text>
-                <Link className="btn btn-primary" to={`/product/${_id}`}>Details</Link>
-            </Card.Body>
-        </Card>
-	)
+        <Col sm={4} className='my-2'>
+            <Card id="productComponent1">
+                <Card.Body>
+                    <Card.Title className='text-primary text-uppercase text-center'>{name}</Card.Title>
+                    <Card.Text>{description}</Card.Text>
+                    <Card.Text className='text-warning'>₱ {price}</Card.Text>
+                    <Card.Footer className='text-center'>
+                    <div className="d-grid gap-2">
+                    <Link className="btn btn-primary" to={`/product/${_id}`}>Details</Link>
+                    </div>
+                    </Card.Footer>
+                </Card.Body>
+            </Card>
+        </Col>
+
+
+
+    );
 }
-
